58e7fab40f9ffa3496a0930efb2d245c0b083a29
Francisco Javier Cañete (UMA) <francis@ic.uma.es>
2025-04-11 11:13:47 +0200

    Update load_channels_github.m

--- load_channels_github.m
+++ load_channels_github.m
@@ -1,4 +1,3 @@
-% Script to load the mat files containing the dataset for the paper
 % "A Close Examination of the Multipath Propagation Stochastic Model for 
 %   Communications over Power Lines"
 % that provides the channels frequency response for three measured channels and
@@ -17,8 +16,8 @@ clear;
 close all;
 
 % Filenames that contains the channels and the fitting for them
-filename_mpm_fitting={'MPM_params_channel_N_min','MPM_params_channel_N_med','MPM_params_channel_N_max'};
-text={'min','med','max'};
+nom_fic_mpm_ajuste={'MPM_params_channel_N_max','MPM_params_channel_N_med','MPM_params_channel_N_min'};
+text={'max','med','min'};
 
 % Parameters
 c = 3*1e8;
@@ -27,9 +26,11 @@ v = c/epsilon_r;
 N_init=2554; % initial value of N (number of pahts)
 
 ind_fig=1;
-for i=1:length(filename_mpm_fitting)
+
+for i=1:length(nom_fic_mpm_ajuste)
+    disp(['Computing NMRSE for channel with N',text{i}]);
     
-    ajuste_mpm=load(['.\data\',filename_mpm_fitting{i}]);
+    ajuste_mpm=load(['.\data\',nom_fic_mpm_ajuste{i}]);
     M=length(ajuste_mpm.fm);
     fm=ajuste_mpm.fm;
     N=ajuste_mpm.N;
@@ -45,17 +46,19 @@ for i=1:length(filename_mpm_fitting)
     NRMSE_dB=zeros(1,num_iter_diezm);
     
     %Generate channel
+    progress_bar=textprogressbar(100,'startmsg','Iteration of the decimation process: ');    
     for ind=1:num_iter_diezm 
+        progress_bar(100*ind/num_iter_diezm);
+                
         P= exp(-(a0+a1*repmat(fm.',1,N(ind))).*repmat(di{ind},M,1)).*exp(-1j*2*pi*repmat(fm.',1,N(ind)).*repmat(di{ind},M,1)/v);
         H_mpm= A(ind)*P*gi{ind};
-        if ind==10 
-            break 
-        end
+
         num = abs(H_measured-H_mpm).^2;
         denom_1 = 1./abs(H_measured).^2;
         NRMSE_dB(ind) = 10*log10((1/M)*num.'*denom_1);
-        
     end
+
+    disp('');
 
     % Plot the channels frequency response amplitude and the modelled ones
     figure(ind_fig); ind_fig = ind_fig + 1;
@@ -68,11 +71,10 @@ for i=1:length(filename_mpm_fitting)
     plot(fm*1e-6,unwrap(angle(H_measured)));grid on;hold on;
     plot(fm*1e-6,unwrap(angle(H_mpm)));grid on;
     xlabel('Frequency (MHz)');ylabel('phase[H(f)] (rad)');
-    
+
     % Plot the normalized RMS value of the fitting error
     figure(ind_fig); ind_fig = ind_fig + 1;
     plot(NRMSE_dB);hold on;grid on;
-    xlabel('Number of decimated paths');ylabel('Average NRMSE (dB)')
-    
+    xlabel('Number of decimated paths');ylabel('Average NRMSE (dB)')   
 end
 
